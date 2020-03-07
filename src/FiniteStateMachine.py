3affd41eb644b9b2589d05a0d420d7baeb1545dc
Alejandro Barrera <alejobarre9@gmail.com>
2020-03-07 00:47:28 -0500

    Automata

--- src/FiniteStateMachine.py
+++ src/FiniteStateMachine.py
@@ -1,9 +1,46 @@
-class FiniteStateMachien:
+class FiniteStateMachine:
+    """
+    A class used to represent a deterministic finite state machine
 
-    def __init__(self):
-        self.map = {}
-        self.startState = None
-        self.endState = []
+    ...
 
+    Attributes
+    ----------
+    Q : list(str)
+    State set
+    S : list(str)
+    Stimuli set
+    R : list(str):
+    Response set
+    initial_state : str
+    FSM initial state
+
+    """
+
+    def __init__(self,Q,S,R,initial_state):
+        map = {}
+        self.Q = Q
+        self.S = S 
+        self.R = R
+        self.initial_state = initial_state
+    def add_state(self, q):
+        pass
+    
+    def add_transition(self, stimuli, initial_s, final_s):
+        pass
+
+    def add_response(self, stimuli, state, response):
+        pass
 
     
+
+
+
+
+
+
+
+        
+
+    
+
---
+++ src/Graph.py
@@ -0,0 +1,8 @@
+class Graph:
+
+    def __init__(self):
+        self.adj_list = {}
+        self.n_vertex = 0
+        self.n_edges = 0
+    
+    
